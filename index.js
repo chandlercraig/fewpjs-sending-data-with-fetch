ecb71243cd1fa2ec7e2496600cb246bc22f77db6
Chandler Craig <hellochandlercraig@gmail.com>
2022-01-30 21:20:54 -0800

    commit tree 45abe3de320458872c661253b4b838f225957418

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+const bodyContainer = document.querySelector('body');
+const idLi = document.createElement('li');
+
+function submitData(userName, userEmail) {
+
+  const formData = {
+    name: `${userName}`,
+    email: `${userEmail}`
+  };
+    
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(obj) {
+      const objID = obj.id;
+      idLi.innerHTML = `${objID}`;
+      bodyContainer.append(idLi);
+    })
+    .catch(function(error) {
+      alert("Bad things! Ragnarők!");
+      const errMsg = error.message;
+      idLi.innerHTML = `${errMsg}`;
+      bodyContainer.append(idLi);
+    });
+}
+
+  
